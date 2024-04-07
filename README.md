d51cb2ff13a1038b167d5dfeaa4d49e1a51f7914
Katya <katyasenior@gmail.com>
2024-04-07 21:18:34 +0100

    responsive design

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # AccessibleGallery
 
+Main Goals Acheived:
+
+1. Display thumbnail images
+2. Display larger image
+3. Handle user interaction.
+
+Stretch Goals:
+
+1.  Keyboard Events (unacheived). Found the pieces but did not allocate enough time to have them function.
+
 Major Bugs:
 
 1.
--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const thumbContainer = document.getElementById("thumbContainer");
 const displayImage = document.getElementById("displayImage");
-
-// the clue for using arrow keys or left and right buttons
-let imageIndex = 0;
+/*const elements = document.querySelectorAll("image");*/
 
 const images = [
   {
     url: "https://images.unsplash.com/photo-1590346320751-876ff414c050?q=80&w=2086&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Space",
     class: "thumbImage",
+    placeholder: "Image",
+    ariaPlaceholder: "Image",
   },
   {
     url: "https://images.unsplash.com/photo-1616529927764-173a543493ee?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Circuit board",
     class: "thumbImage",
+    placeholder: "Image",
+    ariaPlaceholder: "Image",
   },
   {
     url: "https://images.unsplash.com/photo-1624806992221-12d8062c35e5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Cheese",
     class: "thumbImage",
+    placeholder: "Image",
+    ariaPlaceholder: "Image",
   },
 ];
 
@@ -48,6 +52,23 @@ function createBigImage(image) {
 
   displayImage.appendChild(bigImg);
 }
+
+/*elements.forEach(function (element) {
+  element.addEventListener("focus", (e) => {
+    console.log(`Element ${e.currentTarget} is focused`);
+  });
+});
+const ArrowRight = document.querySelector("ArrowRight");
+let imageIndex = 0;
+
+document.getElementById("ArrowRight").addEventListener("keydown", nextImage);
+
+function nextImage() {
+  let imageIndex = imageIndex++;
+  if (imageIndex === 3) {
+    let imageIndex = 0;
+  }
+}*/
 
 createThumbnails();
 createBigImage(images[0]);
